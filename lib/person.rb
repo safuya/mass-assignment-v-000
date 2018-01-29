44c7bc092195730a792130289639661ed3f6db20
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-29 08:00:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight
 
   def initialize(attributes)
     attributes.each { |key, value| self.send(("#{key}="), value) }
